cb542720c467471b8505b5a9f8fa522ba6890b50
Curtis Dyer <dyer85@gmail.com>
2016-05-16 16:55:25 -0700

    Included input/output pins in class.

--- ac.units/ac.py
+++ ac.units/ac.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
 import time
+#import RPi.GPIO as GPIO
 
 class AcUnit:
     "AC unit state"
     __id = 0
+    __in = 0
+    __out = 0
     __value = 0
     __on = 0.0
     __off = 0.0
 
-    def __init__(self, ident, v = 0):
+    def __init__(self, ident, pin_in, pin_out, v = 0):
         self.__id = ident
+        self.__in = pin_in
+        self.__out = pin_out
         self.setVal(v)
 
     # query state
@@ -18,6 +23,10 @@ class AcUnit:
         return self.__value > 0
     def isOff(self):
         return not self.isOn()
+    def getOut(self):
+        return self.__out
+    def getIn(self):
+        return self.__in
     def getVal(self):
         return self.__value
     def getId(self):
@@ -51,7 +60,7 @@ def main():
     timeout = 2.5
 
     # initialize objects
-    units = [AcUnit(1), AcUnit(2, 5), AcUnit(3, 9)]
+    units = [AcUnit(1, 17, 5, 0), AcUnit(2, 27, 6, 5), AcUnit(3, 22, 13, 9)]
     state(units)
     print("Sum: ", sum(v.getVal() for v in units), '\n')
 
@@ -63,6 +72,8 @@ def main():
         msg = "Time on (ID: {0:d}): " if ac.isOn() else "Time off (ID: {0:d}): "
         msg += "{1:03.3f} secs"
         print(msg.format(ac.getId(), ac.getElapsed()))
+
+        print("Pin (input): {0}; Pin (output): {1}\n".format(ac.getIn(), ac.getOut()))
 
 if __name__ == "__main__":
     main()
